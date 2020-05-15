812d54f3eb54f8935148fb35aa584ab907fb676f
rozanurill <64246350+rozanurill@users.noreply.github.com>
2020-05-15 18:16:29 +0700

    Add files via upload
    progress

--- OnboardingTwoActivity.kt
+++ OnboardingTwoActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kantinsehat.gklf.R
+import com.kantinsehat.gklf.sign.SignInActivity
 import com.kantinsehat.gklf.sign.signin.SignInActivity
 import kotlinx.android.synthetic.main.activity_onboarding_two.*
 
